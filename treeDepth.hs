3c45857674e91723fa32f85a0c79cf045f0bdf8e
Tony Fu <tonyfu@fastmail.com>
2023-05-18 12:10:47 +1000

    Simplify count branch

--- treeDepth.hs
+++ treeDepth.hs
@@ -3,11 +3,5 @@
 data Tree a = Empty | Branch a (Tree a) (Tree a)
 leaf x = Branch x Empty Empty
 
-isBalancedTree Empty = True
-isBalancedTree (Branch _ l r) =
-    (countBranches l - countBranches r) == 1
-    || (countBranches r - countBranches l) == 1
-    && isBalancedTree l && isBalancedTree r
-
 countBranches Empty = 0
 countBranches (Branch _ l r) = 1 + l + r
