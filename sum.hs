85ccad5a544e8653a074faf29a3e17a5efca63a2
Tony Fu <tonyfu@fastmail.com>
2023-05-18 15:35:51 +1000

    Simplified some files

--- sum.hs
+++ sum.hs
@@ -1,10 +1,10 @@
--- theme: list
-sum [] = 0
-sum [x] = x
-sum (x:xs) = x + sum xs
+getsum [] = 0
+getsum [x] = x
+getsum (x:xs) = x + getsum xs
 
 check (x:xs)
   | x `mod` 3 == 0 || x `mod` 5 == 0 = x + check xs
   | otherwise = check xs
-
-problem_1 = sum (check [1..999])
+--
+y :: [Int]
+y = check [1..999]
--- totalArea.hs
+++ totalArea.hs
@@ -5,6 +5,3 @@ area (Circle r) = pi * r * r
 area (Rectangle w h) = w * h
 
 shapes = [Rectangle 2.0, Circle 1.0]
-
-totalArea :: Float
-totalArea = sum (map area shapes)
